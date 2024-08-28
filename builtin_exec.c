75c999aec4db39575c9f1e649a33234ebaacc8e6
PascalAmah <pascal.amah10@gmail.com>
2024-08-28 07:12:36 -0700

    debugging

--- builtin_exec.c
+++ builtin_exec.c
@@ -9,27 +9,41 @@
 int change_dir(char **cmd, __attribute__((unused))int st)
 {
 	int value = -1;
-	char cwd[PATH_MAX];
+	char cwd[PATH_MAX], *oldpwd;
 
 	if (cmd[1] == NULL)
 		value = chdir(getenv("HOME"));
 	else if (_strcmp(cmd[1], "-") == 0)
 	{
-		value = chdir(getenv("OLDPWD"));
+		oldpwd = getenv("OLDPWD");
+		if (oldpwd == NULL)
+		{
+			fprintf(stderr, "hsh: cd: OLDPWD not set \n");
+			return (0);
+		}
+		value = chdir(oldpwd);
+		if (value != -1)
+			printf("%s\n", oldpwd);
 	}
 	else
 		value = chdir(cmd[1]);
-
 	if (value == -1)
 	{
 		perror("hsh");
 		return (-1);
 	}
-	else if (value != -1)
+	else
 	{
-		getcwd(cwd, sizeof(cwd));
-		setenv("OLDPWD", getenv("PWD"), 1);
-		setenv("PWD", cwd, 1);
+		if (getcwd(cwd, sizeof(cwd)) != NULL)
+		{
+			setenv("OLDPWD", getenv("PWD"), 1);
+			setenv("PWD", cwd, 1);
+		}
+		else
+		{
+			perror("hsh: getcwd");
+			return (-1);
+		}
 	}
 	return (0);
 }
